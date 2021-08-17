ced3ce935c850ac4a5aef70955bfc32675f24805
Pat O'Connor <patoconnor43@gmail.com>
2021-08-16 17:27:24 -0700

    Add new function to add a new path

--- lua/openapi.lua
+++ lua/openapi.lua
@@ -38,9 +38,53 @@ function M.open_preview()
 end
 
 function M.add_new_path()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local paths = M.get_paths()
+  local maximum_row = 0
+  -- Find the highest row because that corresponds to the last path. Add a new
+  -- one after that.
+  for _, value in pairs(paths) do
+    local row, _, _ = value:end_()
+    if row > maximum_row then
+      maximum_row = row
+    end
+  end
+  -- Add a placeholder path.
+  vim.fn.appendbufline(bufnr, maximum_row+1, '  /placeholder:')
+  -- Move the cursor on top of the 'p' in '/placeholder'.
+  vim.api.nvim_win_set_cursor(0, {maximum_row+2, 3})
+  -- Visually select 'placeholder' to show what was added.
+  vim.cmd('normal! viw')
 end
 
 function M.get_paths()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local ft = vim.api.nvim_buf_get_option(bufnr, "ft")
+  local parser = vim.treesitter.get_parser(bufnr, ft)
+  local tstree = parser:parse()[1]
+  local root = tstree:root()
+  local paths = {}
+  local paths_query = [[
+    (block_mapping_pair key: ((flow_node) @key (eq? @key "paths")) value: (block_node) @value)
+  ]]
+
+
+  local query = vim.treesitter.parse_query(ft, paths_query)
+  for pattern, match, metadata in query:iter_matches(root, bufnr, 0, vim.api.nvim_buf_line_count(bufnr)) do
+    for id, node in pairs(match) do
+      -- skip the flow_node that represents the key.
+      if node:type() == 'block_node' then
+        print('111')
+        paths = get_children_pairs(node, bufnr)
+      end
+    end
+  end
+  return paths
+end
+
+
+function M.test()
+
   local bufnr = vim.api.nvim_get_current_buf()
   local ft = vim.api.nvim_buf_get_option(bufnr, "ft")
   local parser = vim.treesitter.get_parser(bufnr, ft)
@@ -50,33 +94,25 @@ function M.get_paths()
     (block_mapping_pair key: ((flow_node) @key (eq? @key "paths")) value: (block_node) @value)
   ]]
 
-  local query = vim.treesitter.parse_query(ft, paths_query)
-  for pattern, match, metadata in query:iter_matches(root, bufnr, 0, vim.api.nvim_buf_line_count(bufnr)) do
-    for id, node in pairs(match) do
-      -- skip the flow_node that represents the key.
-      if node:type() == 'block_node' then
-          print(vim.inspect(get_children_pairs(node, bufnr)))
-      end
-    end
-  end
-end
 
-
-function M.test()
-
-
-  print(M.get_paths())
-  return
-
-  --local bufnr = vim.api.nvim_get_current_buf()
-  --local ft = vim.api.nvim_buf_get_option(bufnr, "ft")
-  --local parser = vim.treesitter.get_parser(bufnr, ft)
-  --local tstree = parser:parse()[1]
-  --local root = tstree:root()
-  --local paths_query = [[
-  --  (block_mapping_pair key: ((flow_node) @key (eq? @key "paths")) value: (block_node) @value)
-  --]]
-
+  --local paths = M.get_paths()
+  --local last_path
+  --local maximum_row = 1
+  --local maximum_col = 1
+  --for key, value in pairs(paths) do
+  --  local row, col, _ = value:end_()
+  --  if row > maximum_row then
+  --    maximum_row = row
+  --    maximum_col = col
+  --  end
+  --end
+  --vim.fn.appendbufline(bufnr, maximum_row+1, '  /placeholder:')
+  --vim.api.nvim_win_set_cursor(0, {maximum_row+2, 3})
+  ----vim.fn.norm('viw')
+  --vim.cmd('normal! viw')
+  --vim.api.nvim_buf_set_text(bufnr, maximum_row+1, 0, maximum_row+1, 0, {'','lmao'})
+  --print(last_path)
+  --print(maximum)
   --local query = vim.treesitter.parse_query(ft, paths_query)
   --for pattern, match, metadata in query:iter_matches(root, bufnr, 0, vim.api.nvim_buf_line_count(bufnr)) do
   --  for id, node in pairs(match) do
